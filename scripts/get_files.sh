dd63231fd83604d3a66faeca8ceba8231bc6d84b
Heechul Kim <jijisa@iorchard.net>
2023-04-19 15:23:03 +0900

    support burrito branch name in get_files.sh

--- scripts/get_files.sh
+++ scripts/get_files.sh
@@ -3,8 +3,33 @@
 CURRENT_DIR=$( dirname "$(readlink -f "$0")" )
 OFFLINE_FILES_DIR_NAME="files"
 OFFLINE_FILES_DIR="${WORKSPACE}/iso/${OFFLINE_FILES_DIR_NAME}"
-FILES_LIST=${FILES_LIST:-"${WORKSPACE}/files/files.list"}
+FILES_LIST=${FILES_LIST:-"${WORKSPACE}/files/bin.txt"}
+DIST_DIR="${CURRENT_DIR}/dist"
+REL_NAME="${SRC_VER//\//_}"
 
+mkdir -p ${DIST_DIR}
+curl -sLo ${CURRENT_DIR}/git-archive-all.sh \
+  https://raw.githubusercontent.com/fabacab/git-archive-all.sh/master/git-archive-all.sh
+chmod +x ${CURRENT_DIR}/git-archive-all.sh
+
+git clone --recursive -b ${SRC_VER} https://github.com/iorchard/burrito.git \
+  ${DIST_DIR}/burrito
+pushd ${DIST_DIR}/burrito
+  ${CURRENT_DIR}/git-archive-all.sh --prefix burrito-${REL_NAME}/ -- - | \
+    gzip -9 > ${WORKSPACE}/iso/burrito-${REL_NAME}.tar.gz
+popd
+
+pushd ${WORKSPACE}/iso
+  #curl -LO https://github.com/iorchard/burrito/releases/download/${SRC_VER}/burrito-${SRC_VER}.tar.gz
+popd
+
+# extract scripts/{images,bin}.txt to files/
+pushd ${WORKSPACE}/files
+  tar --strip-components=2 -xvzf ${WORKSPACE}/iso/burrito-${REL_NAME}.tar.gz \
+    burrito-${REL_NAME}/scripts/images.txt
+  tar --strip-components=2 -xvzf ${WORKSPACE}/iso/burrito-${REL_NAME}.tar.gz \
+    burrito-${REL_NAME}/scripts/bin.txt
+popd
 # download files
 if [ ! -f "${FILES_LIST}" ]; then
   echo "${FILES_LIST} should exist."
@@ -16,12 +41,3 @@ mkdir -p "${OFFLINE_FILES_DIR}"
 
 wget -x -P "${OFFLINE_FILES_DIR}" -i "${FILES_LIST}"
 
-pushd ${WORKSPACE}/iso
-  curl -LO https://github.com/iorchard/burrito/releases/download/${SRC_VER}/burrito-${SRC_VER}.tar.gz
-popd
-
-# extract scripts/images.txt and mv it to files/burrito_images.txt
-pushd ${WORKSPACE}/files
-  tar --strip-components=2 -xvzf ${WORKSPACE}/iso/burrito-${SRC_VER}.tar.gz \
-    burrito-${SRC_VER}/scripts/images.txt
-popd
