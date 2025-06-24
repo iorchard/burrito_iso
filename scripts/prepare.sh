077b8bfa85f2205f6d85f8141cd009a83da626d6
Heechul Kim <jijisa@iorchard.net>
2025-06-24 18:13:29 +0900

    fix rocky linux download url and epel url to be firewall-friendly

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,13 +2,17 @@
 
 set -ex
 
-# Reset yum repo for the RL version
-#mv /etc/yum.repos.d /etc/yum.repos.d.bak
-#mkdir /etc/yum.repos.d
-#cp ${WORKSPACE}/files/yum.repos.d/* /etc/yum.repos.d/
-#sed -i "s/VERSION/${VER}/g" /etc/yum.repos.d/*.repo
+# Reset yum repo to the RL official site
+mv -f /etc/yum.repos.d /etc/yum.repos.d.bak
+mkdir /etc/yum.repos.d
+cp ${WORKSPACE}/files/yum.repos.d/* /etc/yum.repos.d/
+sed -i "s/VERSION/${VER}/g" /etc/yum.repos.d/*.repo
 
 dnf -y install epel-release
+# Copy files/epel.repo to /etc/yum.repos.d/
+rm /etc/yum.repos.d/epel*.repo
+cp ${WORKSPACE}/files/epel.repo /etc/yum.repos.d/
+
 COMMON_DEPS=(curl findutils git p7zip p7zip-plugins python3.11 python3.11-pip)
 ISO_DEPS=(createrepo_c modulemd-tools genisoimage syslinux)
 IMAGE_DEPS=(podman)
